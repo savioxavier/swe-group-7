19e211403e55910506d62397ab46db3ee30bdce2
Fernando E. Amado-Pupo <139940887+feamado@users.noreply.github.com>
2025-07-13 20:50:47 -0400

    Update GoogleCalendarAPITest.py
    rename incorrect folder type

--- google-calendar-api-test/GoogleCalendarAPITest.py
+++ google-calendar-api-test/GoogleCalendarAPITest.py
@@ -9,7 +9,7 @@ SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Build the full path to the client secret JSON file
-secret_file = 'client_secret.json'
+secret_file = 'client_secrets.json'
 secret_path = os.path.join(BASE_DIR, 'Secrets', secret_file)
 
 # Start OAuth flow
